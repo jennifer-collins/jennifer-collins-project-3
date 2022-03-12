a19833f4edce7febe854fecdd3ddeebd1ab60aee
jennifer-collins <jennifercollins_@hotmail.com>
2022-03-11 21:06:29 -0500

    Import axios, make API call, create randomizer function with button. Create DisplayAffirmations component to display API data on page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import DisplayAffirmations from './DisplayAffirmations';
 
 function App() {
+
+  // Save API data in STATE
+  const [affirmationData, setAffirmationData] = useState({});
+  const [affirmation, setAffirmation] = useState('');
+
+  // Make the API call using axios/useEffect
+  useEffect( () => {
+    axios({
+      url: "https://dulce-affirmations-api.herokuapp.com/affirmation/index",
+      method: "GET",
+      dataResponse: "json"
+    }).then( (dataResponse) => {
+      console.log(dataResponse);
+      
+      // This is saving the data from the API in state
+      setAffirmationData(dataResponse.data)
+    })
+  }, [])
+
+  const handleClick = () => {
+    console.log('I am clicked!');
+    // console.log(affirmationData[2]);
+    // on click randomizer function will run - using math.random to
+    const randomAffirmation = affirmationData[Math.floor(Math.random()*affirmationData.length)]
+    console.log(randomAffirmation);
+    setAffirmation(randomAffirmation.phrase)
+  }
+  console.log(affirmation);
+  
   return (
     <div>
       <header>
@@ -9,6 +41,27 @@ function App() {
         {/* App description */}
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod doloremque labore et obcaecati esse veniam inventore voluptatem, nihil nulla itaque!</p>
       </header>
+
+      <section>
+
+        {/* If I add this to it's own component this is how i would pass the info/function via props */}
+        {/* <button onClick={props.getRandomPhrase}>Get Affirmations</button> */}
+
+        <button onClick={handleClick}>Get Affirmation</button>
+        {/* <Button getRandomPhrase={handleClick}/> */}
+        {/* {
+          affirmationData.map((affirmation) => {
+            return(
+              <p>{affirmation.phrase}</p>
+            )
+          })
+        } */}
+      </section>
+
+      <section>
+        <DisplayAffirmations affirmationPhrase={affirmation}/>
+      </section>
+
     </div>
   );
 }
---
+++ src/DisplayAffirmations.js
@@ -0,0 +1,8 @@
+function DisplayAffirmations(props) {
+    console.log(props);
+    return(
+        <p>{props.affirmationPhrase}</p>
+    )
+}
+
+export default DisplayAffirmations;
